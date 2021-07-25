15a7de090b8c225efee887d9e848e9f62c3b1cba
Nathan Johnson <johnsna7@oregonstate.edu>
2021-07-25 16:10:15 -0500

    added responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   variants: {
