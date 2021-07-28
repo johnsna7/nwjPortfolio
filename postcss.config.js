dfd0d3f1e90790a5135a87013019de4e41533396
Nathan Johnson <johnsna7@oregonstate.edu>
2021-07-27 19:32:44 -0500

    Updated navbar

--- postcss.config.js
+++ postcss.config.js
@@ -3,28 +3,27 @@ const tailwindcss = require('tailwindcss');
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 const plugins = [];
-plugins.push(tailwindcss)
-// plugins.push(tailwindcss('tailwind.config.js'))
-// This is if you want to include your custom config
+plugins.push(tailwindcss('tailwind.config.js'));
+
 
 if (!IS_DEVELOPMENT) {
     const purgecss = require('@fullhuman/postcss-purgecss');
     
     class TailwindExtractor {
         static extract(content) {
-            return content.match(/[A-z0-9-:\/]+/g) || [];
+            return content.match(/[\w-/:]+(?<!:)/g) || []
         }
     }
     
     plugins.push(
         purgecss({
-            content: ['src/*.html'],
+            content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx", "./public/**/*.html"],
             extractors: [
                 {
                     extractor: TailwindExtractor,
                     extensions: ['html']
                 }
-            ],
+            ]
         })
     );
 }
